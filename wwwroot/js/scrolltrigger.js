f28fba1a0f6be3806e6a5cb74381eb012fa610d7
akamateina <teinacsa25@gmail.com>
2023-05-04 16:09:26 +1200

    slide text on scroll

---
+++ wwwroot/js/scrolltrigger.js
@@ -0,0 +1,15 @@
+﻿const wines = [wine1, wine2, wine3, wine4];
+const boxes = [box1, box2, box3, box4];
+
+for (let i = 0; i < wines.length; i++) {
+    gsap.to(wines[i], {
+        scrollTrigger: {
+            trigger: boxes[i],
+            start: "top bottom",
+            end: "center center",
+            //markers: true,   //uncomment to visualize
+            scrub: true,
+        },
+        x: "50%",
+    });
+}
