ed5cadda81c3c8a8422c2355c37ed0426b422ac7
XaviarR <xaviarrehu@gmail.com>
2023-05-22 12:44:04 +1200

    Cart Functionality

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using CapstoneWine.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SendGrid.Helpers.Mail;
+
+namespace CapstoneWine.Controllers
+{
+    public class ProductsController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ProductsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<IActionResult> Index()
+		{
+			// Return an error message if the Wine entity set is null
+			if (_context.Wines == null)
+			{
+				return Problem("Entity set 'ApplicationDbContext.Wines' is null.");
+			}
+			// Otherwise, return the Wines entity set as a view
+			return View(await _context.Wines.ToListAsync());
+		}
+	}
+}
---
+++ Models/CartItem.cs
@@ -0,0 +1,29 @@
+﻿namespace CapstoneWine.Models
+{
+        public class CartItem
+        {
+                public int ProductId { get; set; }
+                public string ProductName { get; set; }
+                public int Quantity { get; set; }
+                public decimal Price { get; set; }
+                public decimal Total
+                {
+                        get { return Quantity * Price; }
+                }
+                public string Image { get; set; }
+
+                public CartItem()
+                {
+                }
+
+                public CartItem(WinesModel wines)
+                {
+                        ProductId = wines.WineID;
+                        ProductName = wines.WineName;
+                        Price = wines.Price;
+                        Quantity = 1;
+                        Image = wines.Image;
+                }
+
+        }
+}
