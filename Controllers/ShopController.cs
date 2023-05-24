2b2843e0133c1d9ebd75dfa3faa00ea7f049fd17
akamateina <teinacsa25@gmail.com>
2023-05-24 19:39:49 +1200

    add category and type filters

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -13,30 +13,16 @@ namespace CapstoneWine.Controllers
 			_context = context;
 		}
 
-		public IActionResult Index(string searchString)
+		public IActionResult Index()
 		{
-			ViewData["CurrentFilter"] = searchString;
-			var wineList = _context.Wines.ToList();
-			var wines = from wine in _context.Wines select wine;
-
-
-			if (!String.IsNullOrEmpty(searchString))
-			{
-				wines = wines.Where(w => w.WineName.Contains(searchString));
-
-				if (wines.Count() == 0)
-				{
-					ViewData["ErrorMessage"] = "No matches found";
-					return View(wineList);
-				}
-
-				return View(wines);
-			}
-
-			var categories = wineList.Select(w => w.Category).Distinct().ToArray();
+			var allWine = _context.Wines.ToList();
+			var categories = allWine.Select(w => w.Category).Distinct().ToArray();
 			ViewData["Categories"] = categories;
 
-			return View(wineList);
+			var types = allWine.Select(w => w.Type).Distinct().ToArray();
+			ViewData["Types"] = types;
+
+			return View(allWine);
 		}
 
 		public IActionResult Product(string searchString)
@@ -58,5 +44,48 @@ namespace CapstoneWine.Controllers
 
 			return RedirectToAction("Index", "Shop");
 		}
+
+		public IActionResult Category(string categoryFilter)
+		{
+			var allWine = _context.Wines.ToList();
+			ViewData["CurrentFilter"] = categoryFilter;
+			var categories = from category in _context.Wines select category;
+
+			if (!String.IsNullOrEmpty(categoryFilter))
+			{
+				categories = categories.Where(c => c.Category.Contains(categoryFilter));
+
+				if (!categories.Any())
+				{
+					return RedirectToAction("Index", "Shop");
+				}
+
+				return View(categories);
+			}
+
+			return View(allWine);
+		}
+
+		public IActionResult Type(string typeFilter)
+		{
+			var allWine = _context.Wines.ToList();
+			ViewData["CurrentFilter"] = typeFilter;
+			var types = from type in _context.Wines select type;
+
+			if (!String.IsNullOrEmpty(typeFilter))
+			{
+				types = types.Where(c => c.Type.Contains(typeFilter));
+
+				if (!types.Any())
+				{
+					return RedirectToAction("Index", "Shop");
+				}
+
+				return View(types);
+			}
+
+			return View(allWine);
+		}
+
 	}
 }
